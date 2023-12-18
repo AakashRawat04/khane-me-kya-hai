20879daa737722d56a908acd28853f2cb4ac29c7
Ash <01tripster@gmail.com>
2023-12-18 20:19:30 +0530

    refactor: menu per day and hour page

--- app/dashboard/[day]/[hour]/layout.tsx
+++ app/dashboard/[day]/[hour]/layout.tsx
@@ -3,7 +3,7 @@ import { SidebarNav } from "@/components/sidebar-nav"
 
 interface WeekLayoutProps {
   children: React.ReactNode
-  params: { week: string; hour: string }
+  params: { day: string; hour: string }
 }
 
 export default function WeekLayout({ children, params }: WeekLayoutProps) {
@@ -11,15 +11,20 @@ export default function WeekLayout({ children, params }: WeekLayoutProps) {
     <>
       <section className="container mt-12 gap-40 bg-background pb-8 pt-6 md:py-10">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          {params.week.toUpperCase()}-
-          {params.hour.charAt(0).toUpperCase() + params.hour.slice(1)}
+          <span className="pr-3 text-green-400">
+            {params.day.toUpperCase()}
+          </span>
+          -
+          <span className="pl-3">
+            {params.hour.charAt(0).toUpperCase() + params.hour.slice(1)}
+          </span>
         </h1>
         <Separator className="mt-4" />
-        <div className="flex flex-row gap-40 pb-8 pt-6 md:py-10">
+        <div className="flex flex-row gap-x-24 pb-8 pt-6 md:py-10">
           <aside className="-mx-4" style={{ minWidth: 0 }}>
-            <SidebarNav week={params.week} hour={params.hour} />
+            <SidebarNav day={params.day} hour={params.hour} />
           </aside>
-          <div className="flex-1 lg:max-w-2xl" style={{ flex: 1 }}>
+          <div className="flex-1 px-10 lg:w-full" style={{ flex: 1 }}>
             {children}
           </div>
         </div>
